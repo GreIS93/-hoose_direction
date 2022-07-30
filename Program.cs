ee0db8edc21b46eee1307b2fea3cf076f2bc2376
GreIS93 <grekovis93@gmail.com>
2022-07-30 14:23:02 +0700

    Three sym void

--- Program.cs
+++ Program.cs
@@ -7,5 +7,10 @@
     Console.WriteLine();
 }
 
+void ThreeSymbolArray(string[] array)
+{
+        for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) Console.Write($"{array[i]}; ");
+}
 
 
